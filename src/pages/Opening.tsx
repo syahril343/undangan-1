7e485d269648669feb19c29fb66b36660a6681b9
syahril343 <syahril.21058@mhs.unesa.ac.id>
2025-08-19 14:36:52 +0700

    setup done

---
+++ src/pages/Opening.tsx
@@ -0,0 +1,15 @@
+
+
+const Opening = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-black overflow-x-hidden text-white">
+      <div
+        className="relative p-4 min-h-screen w-[27rem] bg-cover bg-center bg-red-950"
+      >
+        <h1>bkivbsbv</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Opening
